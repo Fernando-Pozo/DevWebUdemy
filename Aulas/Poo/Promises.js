d470c9ffaccae3d997e2f36f2b13b097e8ac2024
Fernando-Pozo <fernandopozomqs@gmail.com>
2022-02-03 12:28:25 -0300

    promise

--- Aulas/Poo/Promises.js
+++ Aulas/Poo/Promises.js
@@ -23,3 +23,16 @@ const getData = () =>{
 getData()
     .then(value => console.log(value))
     .catch(error => console.log(error))
+//-------------------------------------------------------
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if ( a == 2){
+        resolve('Sucess')
+    } else {
+        reject('Failed')
+    }
+})
+
+p.then((message) => {
+    console.log('This is in the then ' + message)
+})
